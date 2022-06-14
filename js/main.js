13c6d3bdc5318d6714a5f4977cf1ce33effad35b
developeranikhossin <mdmanunbiswas321@gmail.com>
2022-06-14 17:46:15 +0600

    site

---
+++ js/main.js
@@ -0,0 +1,45 @@
+(function ($) {
+    "use strict";
+
+    // slider-area-start 
+
+    $('.slider-active').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        arrows:true,
+        
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+
+    // slider-area-end
+
+})(jQuery);	
