606743ea1060dff325dd6c551d9cfee0e4b41fc7
developeranikhossin <mdmanunbiswas321@gmail.com>
2022-06-15 02:46:53 +0600

    site update

--- js/main.js
+++ js/main.js
@@ -42,4 +42,20 @@
 
     // slider-area-end
 
+
+    // scrollUp-start 
+
+    $.scrollUp({
+        scrollName: 'scrollUp', // Element ID
+        topDistance: '300', // Distance from top before showing element (px)
+        topSpeed: 300, // Speed back to top (ms)
+        animation: 'fade', // Fade, slide, none
+        animationInSpeed: 200, // Animation in speed (ms)
+        animationOutSpeed: 200, // Animation out speed (ms)
+        scrollText: '<i class="fas fa-angle-up"></i>', // Text for element
+        activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+    });
+
+    // scrollUp-end
+
 })(jQuery);	
